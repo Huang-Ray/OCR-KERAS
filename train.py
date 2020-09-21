962012bcfda193653f15fb3873e461ee2e5ea97f
Ray1129 <ab89141@gmail.com>
2020-09-21 23:15:56 +0800

    m

--- train.py
+++ train.py
@@ -3,8 +3,15 @@ from prepare_data import generatorData
 from model import CRNN
 
 import tensorflow as tf
+from keras.layers import Lambda
 from keras.utils import to_categorical
 from keras import backend as K 
+
+
+def ctc_lambda_func(args):
+
+
+    pass
 
 class model_training:
 
@@ -19,16 +26,19 @@ test_datas, test_labels = gendata.gen_Data(batch_size=40)
 
 train_hot = to_categorical(train_labels)
 
-
 input_shapes = np.shape(train_datas)[1:]
-print(input_shapes)
 
 model = CRNN(input_shapes).CRNN_model
 
-ctc_loss = K.ctc_batch_cost(train_hot)
 
+loss = Lambda()
+
+
+
+"""
 model.compile(loss=ctc_loss,
                 optimizer=Adam(lr=0.001),
                 metrics=['accuracy', 'categorical_accuracy'])
+"""
 
 
